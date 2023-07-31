df3ba00ee1f9f6e23cbcdb63bef9da4f742c07cb
Welly <hivoid19@gmail.com>
2023-07-31 15:07:41 +0800

    Fix profile link style & Remove WPCOM override style (#50)
    * Fix profile link text color override by issue
    
    * Remove WPCOM override style

--- src/core.ts
+++ src/core.ts
@@ -288,7 +288,7 @@ export default class Hovercards {
 						href="${ isEditProfile ? 'https://gravatar.com/profiles/edit' : profileUrl }"
 						target="_blank"
 					>
-						<span>${ isEditProfile ? __( i18n, 'Edit your profile' ) : __( i18n, 'View profile' ) }</span>
+						<span class="gravatar-hovercard__profile-link-text">${ isEditProfile ? __( i18n, 'Edit your profile' ) : __( i18n, 'View profile' ) }</span>
 						<svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
 							<path d="M12.6667 8.33338L9.16666 12.1667M12.6667 8.33338L2.66666 8.33338M12.6667 8.33338L9.16666 4.83338" stroke-width="1.5"/>
 						</svg>
